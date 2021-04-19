fa0f270a13532b447107516a4534d1eadedc485f
Sehab-sketch <abdullahalsehab2017@gmail.com>
2021-04-20 00:14:05 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('.slider_area').bxSlider();
+    jQuery('.header-menu').meanmenu({
+      meanMenuContainer: '#mob-nav',
+      meanScreenWidth: 991,
+    });
+  });
